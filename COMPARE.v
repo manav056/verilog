ca2769f1e3f1df03a2ba825923dd74ee8880632c
manav056 <sainimanav05@gmail.com>
2025-04-02 14:38:57 +0530

    Delete COMPARE.v

--- COMPARE.v
+++
@@ -1,8 +0,0 @@
-module COMPARE(lt,gt,eq,din1,din2);
-    input [15:0] din1,din2;
-    output lt,gt,eq;
-    
-    assign lt=din1<din2;
-    assign gt=din1>din2;
-    assign eq=din1==din2;
-endmodule
